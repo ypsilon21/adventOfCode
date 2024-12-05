7c4607eb5c98d6da398d3dd5c0ed0d8d2efc2ef2
ypsilon21 <yannic.momma@gmail.com>
2024-12-05 19:35:51 +0100

    day5

---
+++ day5/src/main/kotlin/day5.kt
@@ -0,0 +1,100 @@
+import java.io.File
+import java.io.FileNotFoundException
+
+fun main(){
+    val fileName = "src/main/resources/input.txt"
+    var rules = mutableListOf<MutableList<String>>()
+    var printedOutput = mutableListOf<String>()
+    var foundEmptyLine = false
+    try {
+        File(fileName).useLines { lines ->
+            lines.forEach {
+                if (it.equals("")) {
+                    foundEmptyLine = !foundEmptyLine
+                } else if (!foundEmptyLine) rules.add(it.split('|').toMutableList())
+                else printedOutput.add(it)
+            }
+        }
+    }
+    catch (e: FileNotFoundException){
+        print("Couldn't find: " + fileName)
+    }
+    val correctOutputs = getCorrectLines(rules, printedOutput, false)
+    val res = addMiddleValues(correctOutputs)
+    print("First Result:  " + res)
+
+    val incorrectOutputs = getCorrectLines(rules, printedOutput, true)
+    val orderedIncorrectOutputs = correctOrder(rules, incorrectOutputs)
+    val res2 = addMiddleValues(orderedIncorrectOutputs)
+    print("\nSecond Result: " + res2)
+}
+
+fun getCorrectLines(rules: MutableList<MutableList<String>>, printedOutput: MutableList<String>, invert: Boolean): MutableList<String> {
+    var res = mutableListOf<String>()
+    for(line in printedOutput){
+        if(invert xor testRules(rules, line)){
+            res.add(line)
+        }
+    }
+    return res
+}
+
+fun testRules(rules: MutableList<MutableList<String>>, line: String): Boolean {
+    if(rules.isEmpty()) return true
+    var rule = rules.first()
+    if(line.contains(rule[0]) && line.contains(rule[1])){
+        if(line.indexOf(rule[0]) < line.indexOf(rule[1])){
+            return testRules(rules.drop(1).toMutableList(), line)
+        }
+        else return false
+    }
+    else return testRules(rules.drop(1).toMutableList(), line)
+}
+
+fun addMiddleValues(lines: MutableList<String>): Int{
+    var res = 0
+    for(line in lines){
+        val line = line.split(",")
+        res += line.get(line.size/2).toInt()
+    }
+    return res
+}
+
+fun correctOrder(rules: MutableList<MutableList<String>>, lines: MutableList<String>): MutableList<String>{
+    var res = mutableListOf<String>()
+    for(line in lines){
+        res.add(correctLine(rules, line))
+    }
+    return res
+}
+
+fun correctLine(rules: MutableList<MutableList<String>>, line: String): String {
+    if(line.isEmpty()) return ""
+    if(testRules(rules, line)) return line
+    var ll = line.split(",")
+    var res = mutableListOf<String>()
+    for(num in ll){
+        insertCorrectly(num, rules, res)
+    }
+    return res.joinToString(",")
+}
+
+fun insertCorrectly(num: String, rules: MutableList<MutableList<String>>, res: MutableList<String>) {
+    for (i in 0..res.size) {
+        var canInsert = true
+        for (rule in rules) {
+            if (rule[1] == num) {
+                // Check if placing `num` here violates any rule
+                if (i < res.size && res.subList(i, res.size).contains(rule[0])) {
+                    canInsert = false
+                    break
+                }
+            }
+        }
+        if (canInsert) {
+            res.add(i, num)
+            return
+        }
+    }
+    res.add(num)
+}
